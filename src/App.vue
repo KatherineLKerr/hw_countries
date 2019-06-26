f3676ad5b62971d07bb6e6fec166e9daf784421a
Katherine Kerr <katherinekerr@protonmail.com>
2019-06-26 16:41:27 +0100

    adds solutions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>countries:</h1>
+    <div class="main-container">
+      <!-- <countries-list :countries="countries"></countries-list> -->
+
+      <select v-model="selectedCountry">
+        <option v-for="(value, key) in countries" :value="value">{{value.name}}</option>
+      </select>
+
+
+
+      <country-detail :country="selectedCountry"></country-detail>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from './components/CountriesList.vue'
+import CountryDetail from './components/CountryDetail.vue'
+import { eventBus} from './main.js'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    }//end of return
+  },//end of data
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country
+    })
+  },//end of mounted
   components: {
-    HelloWorld
-  }
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
+  }//end of component
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
